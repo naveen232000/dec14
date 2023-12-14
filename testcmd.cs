1797b4391629793e043654f2bd7a574d7aea034b
naveen232000 <153808536+naveen232000@users.noreply.github.com>
2023-12-14 15:32:47 +0530

    Add files via upload

---
+++ testcmd.cs
@@ -0,0 +1,40 @@
+using System;
+public class test
+{
+public static void Main()
+{
+Console.WriteLine("From Command Line");
+}//end of main
+}//end of class
+
+/***********************************************************************
+** Visual Studio 2022 Developer Command Prompt v17.8.3
+** Copyright (c) 2022 Microsoft Corporation
+**********************************************************************
+
+C:\Program Files\Microsoft Visual Studio\2022\Enterprise>cd ..
+
+C:\Program Files\Microsoft Visual Studio\2022>cd ..
+
+C:\Program Files\Microsoft Visual Studio>cd ..
+
+C:\Program Files>cd ..
+
+C:\>cd Users\NAVEEN\source\repos\dec14\dec14
+
+C:\Users\NAVEEN\source\repos\dec14\dec14>csc testcmd
+Microsoft (R) Visual C# Compiler version 4.8.0-7.23572.1 (7b75981c)
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+error CS2001: Source file 'C:\Users\NAVEEN\source\repos\dec14\dec14\testcmd' could not be found.
+
+C:\Users\NAVEEN\source\repos\dec14\dec14>csc testcmd.cs
+Microsoft (R) Visual C# Compiler version 4.8.0-7.23572.1 (7b75981c)
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+
+C:\Users\NAVEEN\source\repos\dec14\dec14>testcmd
+From Command Line
+
+C:\Users\NAVEEN\source\repos\dec14\dec14>
+*/
